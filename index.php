33583877fe052de519583a73b1b0bdbe95f76325
Jeroen Seegers <jeroen.seegers+github@gmail.com>
2013-10-13 14:17:09 +0200

    Moved files

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'config.inc.php';
-require_once 'functions.inc.php';
+require_once 'includes/config.inc.php';
+require_once 'includes/functions.inc.php';
 require_once 'omxplayer.class.php';
 
 $sCurrentDirectory = $aConfig['TV_DIR'];
