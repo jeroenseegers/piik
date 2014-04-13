09561e9d14f859d8708930c30afd02adebd3b43b
Jeroen Seegers <jeroen.seegers+github@gmail.com>
2014-04-13 18:41:54 +0200

    Refactor installation process

--- setup.sh
+++ setup.sh
@@ -9,25 +9,13 @@ fi
 printf "" > /tmp/piik_install.log;
 
 # Check if we need to install git
-hash git 2>/dev/null || {
-    printf "%s" "git not found, installing...";
-    sudo apt-get -y install git >> /tmp/piik_install.log && printf "%s\n" " Done";
+maybe_install() {
+    for application in $@; do
+        hash $application 2>/dev/null || {
+            printf "%s" "$application not found, installing...";
+            sudo apt-get -y install $application >> /tmp/piik_install.log && printf "%s\n" " Done";
+        }
+    done
 }
 
-# Check if we need to install nginx
-hash nginx 2>/dev/null || {
-    printf "%s" "nginx not found, installing...";
-    sudo apt-get -y install nginx >> /tmp/piik_install.log && printf "%s\n" " Done";
-}
-
-# Check if we need to install php
-hash php 2>/dev/null || {
-    printf "%s" "php not found, installing...";
-    sudo apt-get -y install php5 >> /tmp/piik_install.log && printf "%s\n" " Done";
-}
-
-# Check if we need to install php-fpm
-hash php5-fpm 2>/dev/null || {
-    printf "%s" "php5-fpm not found, installing...";
-    sudo apt-get -y install php5-fpm >> /tmp/piik_install.log && printf "%s\n" " Done";
-}
+maybe_install git nginx php5 php5-fpm
